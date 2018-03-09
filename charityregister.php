57c21888aab50152d366d2e95a0e6409f532c4d3
Charles Koll <kollch@oregonstate.edu>
2018-03-08 22:05:25 -0800

    Add php register code

---
+++ charityregister.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+include 'database_creds.php';
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (mysqli_connect_errno()) {
+  die(mysqli_connect_error());
+}
+
+$name = mysqli_real_escape_string($conn, $_POST['charity-name-input']);
+$password = mysqli_real_escape_string($conn, $_POST['charity-password-input']);
+$description = mysqli_real_escape_string($conn, $_POST['charity-description-input']);
+$logo = mysqli_real_escape_string($conn, $_POST['charity-logo-input']);
+
+$sql = "INSERT INTO UserAccount VALUES ('" . $username . "', '" . $password . "', '" . $name . "');";
+if (!mysqli_query($conn, $sql)) {
+  die("Failed to create your account. Please try again or contact an administrator.");
+}
+mysqli_close($conn);
+$_SESSION['username'] = $username;
+$_SESSION['is_charity'] = 0;
+header('Location: index.php');
+?>
---
+++ volunteerregister.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+include 'database_creds.php';
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (mysqli_connect_errno()) {
+    die(mysqli_connect_error());
+}
+
+$username = mysqli_real_escape_string($conn, $_POST['volunteer-username-input']);
+$password = mysqli_real_escape_string($conn, $_POST['volunteer-password-input']);
+
+$sql = "INSERT INTO UserAccount VALUES ('" . $username . "', '" . $password . "');";
+if (!mysqli_query($conn, $sql)) {
+  die("Failed to create your account. Please try again or contact an administrator.");
+}
+mysqli_close($conn);
+$_SESSION['username'] = $username;
+$_SESSION['is_charity'] = 0;
+header('Location: index.php');
+?>
